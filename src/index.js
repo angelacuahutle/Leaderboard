fa0cd5d679cd4ad75a00e8ee6eee90523525ac16
angelacuahutle <angela.cuahutle@gmail.com>
2021-12-17 20:48:56 -0600

    Add token value to game id

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
+
+const GameId = 'Gz0BIsu5uJLklNMVUVr5';
+const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${GameId}/scores';
+const call = () => {
+  fetch(
+    API_URL,
+  )
+    .then((response) => response.json())
+    .then((data) => {
+    });
+};
+
+call();
