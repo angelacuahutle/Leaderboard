662ab14a2c63df700f077808e141722f110521c9
angelacuahutle <angela.cuahutle@gmail.com>
2021-12-17 20:55:48 -0600

    Add form

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const GameId = 'Gz0BIsu5uJLklNMVUVr5';
-const API_URL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${GameId}/scores';
+const API_URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${GameId}/scores/`;
 const call = () => {
   fetch(
     API_URL,
