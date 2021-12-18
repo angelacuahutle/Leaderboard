1dcf1e992da98c60b3be617f24c05d7abcc6e2e1
angelacuahutle <angela.cuahutle@gmail.com>
2021-12-17 21:57:09 -0600

    Add try statements to create responses

--- src/index.js
+++ src/index.js
@@ -4,10 +4,27 @@ import 'bootstrap/dist/css/bootstrap.css';
 const GameId = 'Gz0BIsu5uJLklNMVUVr5';
 const API_URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${GameId}/scores/`;
 const tbody = document.getElementById('tbody');
+const searchID = (id) => document.getElementById(id);
+
+const PostToApi = async () => {
+  const user = searchID('user').value;
+  const score = searchID('score').value;
+  const userData = { user, score };
+};
 
 const dData = async () => {
   try {
-    const fetching = await fetch(API_URL);
+    await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json;',
+      },
+      body: JSON.stringify(userData),
+    });
+  } catch (error) {
+    logMyErrors(error);
+  }
+
     const res = await fetching.json();
     const data = res;
     const leaderboard = data.result;
@@ -32,9 +49,18 @@ const dData = async () => {
       tr.appendChild(td);
       tbody.appendChild(tr);
     });
+    
   } catch (error) {
     throw new Error(error.message);
   }
 };
+
+const form = document.getElementById('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  tbody.innerHTML = '';
+  PostToApi();
+  dData();
+});
 
 dData();
